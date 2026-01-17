0f3b3c3368b926de93604e3c9ffc860093f1cc4f
kunikaesp <kunika@espsofttech.com>
2026-01-18 00:33:38 +0530

    asdas

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Router from "./routes/router"
 import './assets/scss/index.scss'
 import { Toaster } from "react-hot-toast"
 import { toastOptions } from "./components/toastConfig"
-
+import '../src/assets/main.css'
 function App() {
   console.log("app Render");
   
