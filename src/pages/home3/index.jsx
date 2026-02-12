67621ab348ae5d5658c223ee447cd3d3bd3953eb
Arcforyou <arcforyou2025@gmail.com>
2026-02-13 01:47:04 +0530

    Updated index.jsx

--- src/pages/home3/index.jsx
+++ src/pages/home3/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './home3.scss';
 import image from '../../utils/helper';
